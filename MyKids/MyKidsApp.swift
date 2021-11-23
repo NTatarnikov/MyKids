e86d2d83337a4f011e35e9ad89a75424c8dd2b1a
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-11-23 21:48:19 +0300

    Initial Commit

---
+++ MyKids/MyKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyKidsApp.swift
+//  MyKids
+//
+//  Created by Nikita Tatarnikov on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
